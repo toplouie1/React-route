95555402faa4022ecfb60528c5f9702763063473
Nima sherpa <Toplouie78@gmail.com>
2021-11-25 14:27:53 -0500

    simple route @6

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+	Link,
+} from "react-router-dom";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Home />
-	</React.StrictMode>,
+	<Router>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/myapps" element={<Navigate to="/learn" />} />
+			<Route path="/learn" element={<Learn />} />
+		</Routes>
+	</Router>,
 	document.getElementById("root")
 );
 
@@ -18,4 +29,14 @@ function Home() {
 	);
 }
 
+function Learn() {
+	return (
+		<div>
+			<h1>Learn</h1>
+			<h4>This is a learning of react - router </h4>
+			<Link to="/learn/course">courses</Link>
+			<Link to="/learn/bundle">bundle</Link>
+		</div>
+	);
+}
 reportWebVitals();
