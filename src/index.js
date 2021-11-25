0b5ff896b2d9e19bded847fbff590407c527e591
Nima sherpa <Toplouie78@gmail.com>
2021-11-25 14:45:29 -0500

    multiple route of component

--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ import {
 	Route,
 	Navigate,
 	Link,
+	Outlet,
 } from "react-router-dom";
 
 ReactDOM.render(
 	<Router>
 		<Routes>
 			<Route path="/" element={<Home />} />
-			<Route path="/myapps" element={<Navigate to="/learn" />} />
-			<Route path="/learn" element={<Learn />} />
+			<Route path="/myApps" element={<Navigate to="/learn" />} />
+			<Route path="/learn" element={<Learn />}>
+				<Route path="courses" element={<Courses />}>
+					<Route path=":courseId" element={CourseId} />
+				</Route>
+				<Route path="bundles" element={<Bundles />} />
+			</Route>
 		</Routes>
 	</Router>,
 	document.getElementById("root")
@@ -34,8 +40,34 @@ function Learn() {
 		<div>
 			<h1>Learn</h1>
 			<h4>This is a learning of react - router </h4>
-			<Link to="/learn/course">courses</Link>
-			<Link to="/learn/bundle">bundle</Link>
+			<Link to="/learn/courses">Courses</Link>
+			<br />
+			<Link to="/learn/bundles">Bundles</Link>
+			<Outlet />
+		</div>
+	);
+}
+
+function Courses() {
+	return (
+		<div>
+			<h1>This one is My Course List </h1>
+			<h4>Courses Card</h4>
+		</div>
+	);
+}
+function Bundles() {
+	return (
+		<div>
+			<h1>This one is My Bundle List </h1>
+			<h4>Bundles Card</h4>
+		</div>
+	);
+}
+function CourseId() {
+	return (
+		<div>
+			<h1>URL Params is : ___ </h1>
 		</div>
 	);
 }
